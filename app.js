f39c39cc40bc77399c39687fd38576508ca8b698
lucrampro <lucienramassamy@gmail.com>
2018-06-26 23:33:26 +0200

    Ajout d'un bounce en fin d'animation

--- app.js
+++ app.js
@@ -21,16 +21,35 @@ TweenMax.to(".c", 1, {width:"100%"});
 TweenMax.to(".d", 1, {width:"100%"});
 TweenMax.to(".e", 1, {width:"100%",onComplete:function(){
 TweenMax.to(".a", 0.6, {height:200});
-TweenMax.to(".b", 1.6, {height:200});
-TweenMax.to(".c", 2.6, {height:200});
-TweenMax.to(".d", 3.6, {height:200});
-TweenMax.to(".e", 4.6, {height:200,onComplete:function(){
+TweenMax.to(".b", 0.6, {height:200});
+TweenMax.to(".c", 0.6, {height:200});
+TweenMax.to(".d", 0.6, {height:200});
+TweenMax.to(".e", 0.6, {height:200,onComplete:function(){
 TweenMax.to(".a", 0.2, {width:200,});
 TweenMax.to(".b", 0.4, {width:200,});
 TweenMax.to(".c", 0.6, {width:200,});
 TweenMax.to(".d", 0.8, {width:200,});
-TweenMax.to(".e", 1, {width:200,});
-TweenMax.to(".logo", 1, {right:"0",});
+TweenMax.to(".e", 1, {width:200,onComplete:function(){
+    
+    
+    TweenMax.to(".a", 0.5, {width:"50%", ease: Bounce.easeOut});
+    TweenMax.to(".b", 0.5, {width:"50%", ease: Bounce.easeOut});
+    TweenMax.to(".c", 0.5, {width:"50%", ease: Bounce.easeOut});
+    TweenMax.to(".d", 0.5, {width:"50%", ease: Bounce.easeOut});
+    TweenMax.to(".e", 0.5, {width:"50%", ease: Bounce.easeOut, function(){
+        
+     
+        
+        
+        
+        
+        
+        
+        
+    }});
+    
+}});
+
 }});
 }});
 }});
