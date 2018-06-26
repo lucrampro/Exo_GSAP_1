28c3cd840461ab09b499b0c8b26c4fdd0152ed71
lucrampro <lucienramassamy@gmail.com>
2018-06-26 17:44:04 +0200

    supression du logo et test d'animation: 4rectangle qui se retracte sur la gauche

--- app.js
+++ app.js
@@ -1,3 +1,10 @@
-alert("js ok");
 
-TweenMax.to(".logo", 2, {width:400});
+
+TweenMax.to(".a", 0.2, {width:200});
+TweenMax.to(".b", 0.4, {width:200});
+TweenMax.to(".c", 0.6, {width:200});
+TweenMax.to(".d", 0.8, {width:200});
+TweenMax.to(".e", 1, {width:200});
+
+
+
