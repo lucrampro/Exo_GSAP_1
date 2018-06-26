05cc321eb58806829c99b358d958bb470437b894
lucrampro <lucienramassamy@gmail.com>
2018-06-26 18:07:22 +0200

    L'animation evolue

--- app.js
+++ app.js
@@ -1,10 +1,37 @@
 
-
+//ETAPE 1 A GAUCHE 1
 TweenMax.to(".a", 0.2, {width:200});
 TweenMax.to(".b", 0.4, {width:200});
 TweenMax.to(".c", 0.6, {width:200});
 TweenMax.to(".d", 0.8, {width:200});
 TweenMax.to(".e", 1, {width:200});
+//ETAPE 2 en HAUT 2.2
+TweenMax.to(".a", 1, {height:0}).delay(1.1);
+TweenMax.to(".b", 1, {height:0}).delay(1.1);
+TweenMax.to(".c", 1, {height:0}).delay(1.1);
+TweenMax.to(".d", 1, {height:0}).delay(1.1);
+TweenMax.to(".e", 1, {height:0}).delay(1.1);
+//ETAPE 2 en bas 4.5
+TweenMax.to(".a", 0.2, {height:120}).delay(2.3);
+TweenMax.to(".b", 0.4, {height:120}).delay(2.3);
+TweenMax.to(".c", 0.6, {height:120}).delay(2.3);
+TweenMax.to(".d", 0.8, {height:120}).delay(2.3);
+TweenMax.to(".e", 1, {height:120}).delay(2.3);
+//ETAPE 2 en bas 4.5
+TweenMax.to(".a", 1, {width:"100%"}).delay(3.4);
+TweenMax.to(".b", 1, {width:"100%"}).delay(3.4);
+TweenMax.to(".c", 1, {width:"100%"}).delay(3.4);
+TweenMax.to(".d", 1, {width:"100%"}).delay(3.4);
+TweenMax.to(".e", 1, {width:"100%"}).delay(3.4);
+//ETAPE 2 en bas 8
+TweenMax.to(".a", 0.6, {height:200}).delay(4.5);
+TweenMax.to(".b", 1.6, {height:200}).delay(4.5);
+TweenMax.to(".c", 2.6, {height:200}).delay(4.5);
+TweenMax.to(".d", 3.6, {height:200}).delay(4.5);
+TweenMax.to(".e", 4.6, {height:200}).delay(4.5);
+
+
+
 
 
 
